c9a0688d6194fc23d836876af697afbc51a14036
WJ <bintas@163.com>
2017-12-26 22:55:48 +0800

    '2'

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -144,5 +144,6 @@ if os.getcwd()=='/app':
 
     # 静态资产配置
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    # STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
